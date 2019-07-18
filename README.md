5f75dcd5e07647f98391a8685f584f3dd0d94d24
Egor <7gorbachevm@gmail.com>
2019-07-18 23:15:16 +0300

    Dashbot logs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Auth URL: https://discordapp.com/oauth2/authorize?&client_id=598617089585971230&scope=bot&permissions=2048
+### toastmaster-bot
+Discord bot for toastmasters. Auth URL: https://discordapp.com/oauth2/authorize?&client_id=598617089585971230&scope=bot&permissions=2048
+
+### Installation
+- `git clone`
+- `npm install`
+- `cp .env.example .env`
+- `npm run start:local`
+
+### Production
+- `npm run start:prod`
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import * as Discord from 'discord.js';
 import { RateSpeechesClient } from './rate-speeches-client';
 import { GoogleTranslator } from './google-translator';
-import { formatMessage } from "./format-message";
+import { formatMessage } from './format-message';
+const createDashbot = require('dashbot');
 
 require('dotenv').config();
 
 const discordSecret = process.env.DISCORD_SECRET;
 const googleProjectId = process.env.GOOGLE_PROJECT_ID!;
+const dashbotApiToken = process.env.DASHBOT_API_TOKEN!;
 
 const googleTranslator = new GoogleTranslator(googleProjectId);
 const rateSpeechesClient = new RateSpeechesClient();
 const discord = new Discord.Client();
+const dashbot = createDashbot(dashbotApiToken).universal;
 
 const greeting = 'Привет, я могу брать темы для обсуждения с сайта ratespeeches.com и прогонять их через Гугл-переводчик.';
 const helpMessage = `\n\nДоступные команды:\n\`roll\` или \`ролл\` - запросить список случайных тем\n\`help\` - помощь`;
@@ -25,8 +28,13 @@ discord.on('message', async (message: Discord.Message) => {
   if (message.author.bot || message.channel.type !== directMessage) {
     return;
   }
+  dashbot.logIncoming({ text: message.content, userId: message.author.id });
   if (message.content === 'help') {
-    return message.reply(`${greeting} ${helpMessage}`);
+    const outgoingMessage = `${greeting} ${helpMessage}`;
+    return Promise.all([
+      message.reply(outgoingMessage),
+      dashbot.logOutgoing({ text: outgoingMessage, userId: message.author.id })
+    ]);
   }
   if (/roll|ролл|hi/i.test(message.content)) {
     await message.channel.startTyping();
@@ -36,13 +44,20 @@ discord.on('message', async (message: Discord.Message) => {
     }));
     const messages = formatMessage(topics, translations);
     await message.reply(messages);
-    await message.channel.stopTyping();
+    await Promise.all([
+      message.channel.stopTyping(),
+      dashbot.logOutgoing({ text: messages, userId: message.author.id })
+    ]);
   } else if (message.content) {
-    await message.reply(`Не могу распознать команду. ${helpMessage}`)
+    const outgoingMessage = `Не могу распознать команду. ${helpMessage}`;
+    await Promise.all([
+      message.reply(outgoingMessage),
+      dashbot.logOutgoing({ text: outgoingMessage, userId: message.author.id })
+    ]);
   }
 });
 
-discord.on('error', async error => {
+discord.on('error', error => {
   console.error(error);
 });
 
